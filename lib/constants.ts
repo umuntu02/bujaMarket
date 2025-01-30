456ef8c936db74068239c059e555e8474c00ee2d
Devlin King IRUMVA <dev-irumva@MacBook-Pro-de-Devlin.local>
2025-01-30 20:16:39 +0100

    page de bienvenue

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "BujaMarket";
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION;
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN;
