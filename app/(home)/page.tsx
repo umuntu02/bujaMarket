f1d62e0c3d523db70606870b6f280528a2e31f54
Devlin King IRUMVA <dev-irumva@MacBook-Pro-de-Devlin.local>
2025-02-01 16:32:48 +0100

    Ajout de hover sur les products

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,18 @@
 import { HomeCard } from "@/components/shared/home/home-card";
 import { HomeCarousel } from "@/components/shared/home/home-carousel";
+import ProductSlider from "@/components/shared/product/product-slider";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   getAllCategories,
+  getProductsByTag,
   getProductsForCard,
 } from "@/lib/actions/product.actions";
 import data from "@/lib/data";
 import { toSlug } from "@/lib/utils";
 
 export default async function Page() {
+  const todaysDeals = await getProductsByTag({ tag: "todays-deal" });
+
   const categories = (await getAllCategories()).slice(0, 4);
   const newArrivals = await getProductsForCard({
     tag: "new-arrival",
@@ -65,6 +70,12 @@ export default async function Page() {
       <HomeCarousel items={data.carousels} />
       <div className="md:p-4 md:space-y-4 bg-border">
         <HomeCard cards={cards} />
+
+        <Card className="w-full rounded-none">
+          <CardContent className="p-4 items-center gap-3">
+            <ProductSlider title={"Today's Deals"} products={todaysDeals} />
+          </CardContent>
+        </Card>
       </div>
     </>
   );
