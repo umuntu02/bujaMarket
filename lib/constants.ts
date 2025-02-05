9925903526c48c3a07944feae75ed11dcab45d3e
Devlin King IRUMVA <dev-irumva@MacBook-Pro-de-Devlin.local>
2025-02-05 17:18:26 +0100

    9 ajout de la page de detail produit

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,4 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "BujaMarket";
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION;
 export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN;
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9);
